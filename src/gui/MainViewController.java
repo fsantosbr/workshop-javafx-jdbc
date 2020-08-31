be809257a255e71c5a2b035bf7d880c0c1bac1df
Fabio Alves <fsantosinfo@gmail.com>
2020-08-30 22:19:42 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -40,14 +41,19 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		// loadView("/gui/DepartmentList.fxml");
-		loadView2("/gui/DepartmentList.fxml"); // this line is only for testing. We'll delete it.
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
+		// Added a second argument to the method. A function (lambda).
+		
 	}
 	
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
+		// As the About screen doens't have anything to be done/shown, we put a function to return nothing in the loadView() arg
 	}
 	
 	
@@ -58,7 +64,8 @@ public class MainViewController implements Initializable {
 	}
 	
 	// Method to call another screen. We'll pass a full path name of a file .fxml as an argument
-	private synchronized void loadView(String absoluteName) {
+	// now this is a generic function. function of type T
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		// synchronized will guarantee that the screen change will occur and not stopped.
 		try {
 			FXMLLoader  loader = new FXMLLoader(getClass().getResource(absoluteName)); // object to load a screen
@@ -81,6 +88,11 @@ public class MainViewController implements Initializable {
 			// now we're adding all the elements cause we want to keep the menu across all screens
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
 		}
 		catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading View", e.getMessage(), AlertType.ERROR);
@@ -88,39 +100,5 @@ public class MainViewController implements Initializable {
 	}
 
 	
-	// this method is only for testing. Let's delete it after
-	private synchronized void loadView2(String absoluteName) {
-		// synchronized will guarantee that the screen change will occur and not stopped.
-		try {
-			FXMLLoader  loader = new FXMLLoader(getClass().getResource(absoluteName)); // object to load a screen
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene(); // calling an scene from the Main class
-			
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			// mainScene.getRoot() will get the first element from the MainView.fxml, The ScrollPane. 
-			// Then, we make a casting to ScrollPane class.
-			// .getContent() will get the content of the ScrollPane. It's already a reference to what it's inside of the ScrollPane, the VBox.
-			// Then, we make a casting to VBox
-			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			// we must keep the menus. So, we'll store a reference of it.	
-			// Getting the first children in the VBox.
-			
-			mainVBox.getChildren().clear(); // Clearing all children of mainVBox
-			
-			// now we're adding all the elements cause we want to keep the menu across all screens
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading View", e.getMessage(), AlertType.ERROR);
-		}
-	}
 	
 }
