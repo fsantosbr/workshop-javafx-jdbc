90e49810dbfd874e1f299317997d2eb58603d1df
Fabio Alves <fsantosinfo@gmail.com>
2020-09-03 01:05:07 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,19 +3,27 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 //class to be the Controller of the elements on the DepartmentForm screen (from DepartmenForm.fxml)
 public class DepartmentFormController implements Initializable {
 
 	
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -37,16 +45,41 @@ public class DepartmentFormController implements Initializable {
 		this.entity = entity;
 	}
 	
-	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("saving");
-		
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
+	
 	@FXML
-	public void onBtCancelAction() {
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close(); //this line will close the current screen
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error Saving Object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
 		System.out.println("Canceling");
+		Utils.currentStage(event).close(); //this line will close the current screen
 	}
 	
 	
