c8ea9d58fe4b453d5d637fcbebfbad6778c8aa54
Fabio Alves <fsantosinfo@gmail.com>
2020-09-05 18:28:51 -0300

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,10 @@
 package gui.util;
 
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 // class to show alerts for some kind of messages (e.g. error messages, warning messages)
 public class Alerts {
@@ -12,5 +15,15 @@ public class Alerts {
 		alert.setHeaderText(header);
 		alert.setContentText(content);
 		alert.show();
+	}
+	
+	
+	// method will be used to show a confirmation message to the user in order to avoid mistake when deleting a row
+	public static Optional<ButtonType> showConfirmation(String title, String content){
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
 	}
 }
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -25,4 +25,8 @@ public class DepartmentService {
 		}
 	}
 	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
+	}
+	
 }
