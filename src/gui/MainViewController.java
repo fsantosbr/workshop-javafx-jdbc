7e285d10c471f8955189ccab24cd75e1ce983c33
Fabio Alves <fsantosinfo@gmail.com>
2020-09-05 18:44:40 -0300

    SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 // class to be the Controller of the elements from screen (from MainView.fxml)
 public class MainViewController implements Initializable {
@@ -34,8 +35,11 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemSellerAction() {
-	
-		System.out.println("onMenuItemSellerAction");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
+		// Added a second argument to the method. A function (lambda).
 	}
 	
 	
---
+++ src/gui/SellerListController.java
@@ -0,0 +1,194 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+
+//class to be the Controller of the elements from the SellerList screen (from SellerList.fxml)
+public class SellerListController implements Initializable, DataChangeListener {
+
+	
+	private SellerService service;
+	// We won't instantiate SellerService here because we wanna avoid the strong relation. We'll use a public set method
+	
+	@FXML
+	private TableView<Seller> tableViewSeller;
+	
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId; // handling the id (an integer)
+	
+	@FXML
+	private TableColumn<Seller, String> tableColumnName; // handling the name (a string)
+	
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnEDIT;
+	
+	
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnREMOVE;
+	
+	
+	@FXML
+	private Button btNew;
+	
+	private ObservableList<Seller> obsList; // we'll load the Sellers in this.
+	
+	
+	@FXML
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		Seller obj = new Seller();
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+	}
+	
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	
+	@Override
+	public void initialize(URL aul, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		// The follow code will adjust the size of the table view screen to be the same as the all screens 
+		Stage stage = (Stage) Main.getMainScene().getWindow(); // getting the reference of the scene
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	// This method will access the service, load the Sellers and pass them to the ObservableList<T>
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Seller> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewSeller.setItems(obsList);
+		initEditButtons();
+		initRemoveButtons();
+	}
+	
+	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+//		try {
+//			
+//			FXMLLoader  loader = new FXMLLoader(getClass().getResource(absoluteName)); // object to load a screen
+//			Pane pane = loader.load();
+//			
+//			SellerFormController controller = loader.getController();
+//			controller.setSeller(obj);
+//			controller.setSellerService(new SellerService());
+//			controller.subscriveDataChangeListener(this); // here we're subscribing ourselves
+//			controller.updateFormData();
+//			
+//			Stage dialogStage = new Stage();
+//			dialogStage.setTitle("Enter Seller data");
+//			dialogStage.setScene(new Scene(pane));
+//			dialogStage.setResizable(false); // the window cannot be resizeble
+//			dialogStage.initOwner(parentStage);
+//			dialogStage.initModality(Modality.WINDOW_MODAL);
+//			dialogStage.showAndWait();
+//			
+//		}
+//		catch(IOException e) {
+//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+//		}
+	}
+
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+		// This method is from DataChangeListener Interface.
+		// when a change occurs, it'll trigger our update method created here
+		
+	}
+	
+	
+	// this method will put an edit button in each line/row of the table 
+	// the next method was taken from this link: https://stackoverflow.com/questions/32282230/fxml-javafx-8-tableview-make-a-delete-button-in-each-row-and-delete-the-row-a
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("edit");
+			
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+					event -> createDialogForm(obj, "/gui/SellerForm.fxml",Utils.currentStage(event)));
+			}
+		});
+	}
+	
+	// this method will put a delete button in each line/row of the table 
+	// the next method was taken from this link: https://stackoverflow.com/questions/32282230/fxml-javafx-8-tableview-make-a-delete-button-in-each-row-and-delete-the-row-a
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("remove");
+			
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+
+	private void removeEntity(Seller obj) {
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+		
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+	}
+
+}
