1f4d739cae327e1d5ea76ff43ef6cc04df33cd9a
Fabio Alves <fsantosinfo@gmail.com>
2020-09-06 22:54:39 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,153 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+//class to be the Controller of the elements on the SellerForm screen (from SellerForm.fxml)
+public class SellerFormController implements Initializable {
+
+	
+	private Seller entity;
+	
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	// command to add obj to be listened
+	// this method will add to a list whatever wants to be updated
+	public void subscriveDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners(); // calling the method that will update the content automatically
+			Utils.currentStage(event).close(); //this line will close the current screen
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		
+		catch (DbException e) {
+			Alerts.showAlert("Error Saving Object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	// this method will notify all subscribers of the DataChangeListener about a change
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener: dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+	}
+	
+
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		// .trim() will delete any blank space around
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "field can't be empty");
+		}
+		obj.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+			// we let to throw our exception later. First we checked if there's any error in our Map, if it is true, we throw our exception
+		}
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		System.out.println("Canceling");
+		Utils.currentStage(event).close(); //this line will close the current screen
+	}
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet(); // .keySet() method returns a Set<T> 
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+
+}
