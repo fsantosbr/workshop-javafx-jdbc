8667a18ab1acae7727b80484741cef203de49c7b
Fabio Alves <fsantosinfo@gmail.com>
2020-09-06 23:27:24 -0300

    TextField & DatePicker

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,55 +1,58 @@
 package gui.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 public class Utils {
 
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
-	
+
 	public static Integer tryParseToInt(String str) {
 		try {
 			return Integer.parseInt(str);
-		}
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			return null;
 		}
 	}
-	
-	
+
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
 			TableCell<T, Date> cell = new TableCell<T, Date>() {
 				private SimpleDateFormat sdf = new SimpleDateFormat(format);
-				
+
 				@Override
 				protected void updateItem(Date item, boolean empty) {
 					super.updateItem(item, empty);
-						if (empty) {
-							setText(null);
-						} else {
-							setText(sdf.format(item));
-						}
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
 				}
 			};
 			return cell;
 		});
 	}
+
 	
 	
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
 		tableColumn.setCellFactory(column -> {
 			TableCell<T, Double> cell = new TableCell<T, Double>() {
-				
+
 				@Override
 				protected void updateItem(Double item, boolean empty) {
 					super.updateItem(item, empty);
@@ -57,11 +60,41 @@ public class Utils {
 						setText(null);
 					} else {
 						Locale.setDefault(Locale.US);
-						setText(String.format("%."+decimalPlaces+"f", item));
+						setText(String.format("%." + decimalPlaces + "f", item));
 					}
 				}
 			};
 			return cell;
+		});
+	}
+	
+	
+	// This method idea was taken from https://stackoverflow.com/questions/26831978/javafx-datepicker-getvalue-in-a-specific-format
+	// This method will lets us use the date as we want
+	public static void formatDatePicker(DatePicker datePicker, String format) {
+		datePicker.setConverter(new StringConverter<LocalDate>() {
+			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+			{
+				datePicker.setPromptText(format.toLowerCase());
+			}
+
+			@Override
+			public String toString(LocalDate date) {
+				if (date != null) {
+					return dateFormatter.format(date);
+				} else {
+					return "";
+				}
+			}
+
+			@Override
+			public LocalDate fromString(String string) {
+				if (string != null && !string.isEmpty()) {
+					return LocalDate.parse(string, dateFormatter);
+				} else {
+					return null;
+				}
+			}
 		});
 	}
 }
